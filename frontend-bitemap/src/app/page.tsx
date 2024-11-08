0ffef80556bf0fc34db3dac8b13e0f25adc9bfbd
Mukta Deshmukh <muktadeshmukh@Muktas-MacBook-Air.local>
2024-11-08 14:48:11 -0500

    Save my changes before pulling

--- frontend-bitemap/src/app/page.tsx
+++ frontend-bitemap/src/app/page.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Footer from './components/footer';
 import Header from './components/header';
 import Body from './components/body_hp';
-import SearchBar from './components/SearchBar';
+import NewHeader from "./components/newheader";
+import NewBody from "./components/newbody_hp";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-      
-      <Body/>
+      {/*<Header />*/}
+      <NewHeader/>
+      <NewBody/>
+      {/*<Body/>*/}
       <Footer />
     </div>
   );
